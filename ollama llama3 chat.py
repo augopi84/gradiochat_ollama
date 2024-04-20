caf988adb8c50f18e0baa470b7adcb3792e11639
Gopinath Chinnakaruppan <augopi84@gmail.com>
2024-04-20 13:00:57 -0400

    Add files via upload

---
+++ ollama llama3 chat.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import ollama
+
+def format_history(msg: str, history: list[list[str, str]], system_prompt: str):
+    chat_history = [{"role": "system", "content":system_prompt}]
+    for query, response in history:
+        chat_history.append({"role": "user", "content": query})
+        chat_history.append({"role": "assistant", "content": response})  
+    chat_history.append({"role": "user", "content": msg})
+    return chat_history
+
+def generate_response(msg: str, history: list[list[str, str]], system_prompt: str):
+    chat_history = format_history(msg, history, system_prompt)
+    response = ollama.chat(model='llama3', stream=True, messages=chat_history)
+    message = ""
+    for partial_resp in response:
+        token = partial_resp["message"]["content"]
+        message += token
+        yield message
+
+chatbot = gr.ChatInterface(
+                generate_response, chatbot=gr.Chatbot(
+                        height="64vh"
+                    ),
+                additional_inputs=[
+                    gr.Textbox(
+                        "Behave as if you are assistant.",
+                        label="System Prompt"
+                    )
+                ],
+                title="Gradio Llama3 with Ollama'",
+                description="Ask me",
+                theme="soft",
+                submit_btn="Feeling lucky",
+                retry_btn="🔄 Regenerate Response",
+                undo_btn="↩ Delete Previous",
+                clear_btn="🗑️ Clear Chat"
+)
+
+chatbot.launch()
